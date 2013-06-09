136a169ef25ebb0c5d2268ac8608ada18ad38a84
argl <andi@null2.net>
2013-06-09 21:48:32 +0200

    reduced data for developmen, only countries beginnign with "a", cleanup

--- js/main.js
+++ js/main.js
@@ -29,70 +29,64 @@ Zepto(function($){
   svg.append("circle")
       .attr("r", outerRadius);
 
-  // d3.csv('data/data.csv', function(data) {
-  //   console.log(data);
-  // });
+  d3.json("data/countries.json", function(countries) {
+    d3.json("data/matrix_1990.json", function(matrix) {
 
-  d3.json("data/matrix_1990.json", function(matrix) {
+      // Compute the chord layout.
+      layout.matrix(matrix);
 
-    // Compute the chord layout.
-    layout.matrix(matrix);
+      // Add a group per neighborhood.
+      var group = svg.selectAll(".group")
+          .data(layout.groups)
+        .enter().append("g")
+          .attr("class", "group")
+          .on("mouseover", mouseover);
 
-    // Add a group per neighborhood.
-    var group = svg.selectAll(".group")
-        .data(layout.groups)
-      .enter().append("g")
-        .attr("class", "group")
-        .on("mouseover", mouseover);
-
-    // Add a mouseover title.
-    // group.append("title").text(function(d, i) {
-    //   return cities[i].name + ": " + formatPercent(d.value) + " of origins";
-    // });
-
-    // Add the group arc.
-    var groupPath = group.append("path")
-        .attr("id", function(d, i) { return "group" + i; })
-        .attr("d", arc)
-        .style("fill", function(d, i) { return '#dd0000'; });
-
-    // Add a text label.
-    var groupText = group.append("text")
-        .attr("x", 6)
-        .attr("dy", 15);
-
-    // groupText.append("textPath")
-    //     .attr("xlink:href", function(d, i) { return "#group" + i; })
-    //     .text(function(d, i) { return cities[i].name; });
-
-    // Remove the labels that don't fit. :(
-    groupText.filter(function(d, i) { return groupPath[0][i].getTotalLength() / 2 - 16 < this.getComputedTextLength(); })
-        .remove();
-
-    // Add the chords.
-    var chord = svg.selectAll(".chord")
-        .data(layout.chords)
-      .enter().append("path")
-        .attr("class", "chord")
-        .style("fill", function(d) { return '#ff0000'; })
-        .attr("d", path);
-
-    // Add an elaborate mouseover title for each chord.
-    // chord.append("title").text(function(d) {
-    //   return cities[d.source.index].name +
-    //       " → " + cities[d.target.index].name +
-    //       ": " + formatPercent(d.source.value) +
-    //       "\n" + cities[d.target.index].name +
-    //       " → " + cities[d.source.index].name +
-    //       ": " + formatPercent(d.target.value);
-    // });
-
-    function mouseover(d, i) {
-      chord.classed("fade", function(p) {
-        return p.source.index != i && p.target.index != i;
+      // Add a mouseover title.
+      group.append("title").text(function(d, i) {
+        return countries[i];
       });
-    }
+
+      // Add the group arc.
+      var groupPath = group.append("path")
+          .attr("id", function(d, i) { return "group" + i; })
+          .attr("d", arc)
+          .style("fill", function(d, i) { return '#dd0000'; });
+
+      // Add a text label.
+      var groupText = group.append("text")
+          .attr("x", 6)
+          .attr("dy", 15);
+
+      groupText.append("textPath")
+          .attr("xlink:href", function(d, i) { return "#group" + i; })
+          .text(function(d, i) { return countries[i]; });
+
+      // Remove the labels that don't fit. :(
+      groupText.filter(function(d, i) { return groupPath[0][i].getTotalLength() / 2 - 25 < this.getComputedTextLength(); })
+          .remove();
+
+      // Add the chords.
+      var chord = svg.selectAll(".chord")
+          .data(layout.chords)
+        .enter().append("path")
+          .attr("class", "chord")
+          .style("fill", function(d) { return '#ff0000'; })
+          .attr("d", path);
+
+      // Add an elaborate mouseover title for each chord.
+      chord.append("title").text(function(d) {
+        return countries[d.source.index];
+      });
+
+      function mouseover(d, i) {
+        chord.classed("fade", function(p) {
+          return p.source.index != i && p.target.index != i;
+        });
+      }
+    });
   });
+
 
 
 });
