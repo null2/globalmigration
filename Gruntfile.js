68b99cc97104917a9d509693d6189cfaa07f80fd
Johannes J. Schmidt <schmidt@netzmerk.com>
2014-03-17 15:27:59 +0100

    minifiy

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,13 +31,71 @@ module.exports = function(grunt) {
         dest: 'json/migrations.json'
       }
     },
-    clean: ['tmp', 'json']
+    concat: {
+      js: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'javascripts/modernizr.js',
+          'javascripts/d3.v3.min.js',
+          'lib/countrymerge.js',
+          'lib/layout.js',
+          'lib/chord.js',
+          'lib/timeline.js',
+          'lib/chart.js'
+        ],
+        dest: 'dist/app-v1.js'
+      },
+      css: {
+        options: {
+          separator: '\n'
+        },
+        src: [
+          'stylesheets/normalize.css',
+          'stylesheets/styles.css'
+        ],
+        dest: 'dist/app-v1.css'
+      }
+    },
+    uglify: {
+      js: {
+        files: {
+        'dist/app-v1.min.js': ['dist/app-v1.js']
+        }
+      }
+    },
+    cssmin: {
+      css: {
+        src: 'dist/app-v1.css',
+        dest: 'dist/app-v1.min.css'
+      }
+    },
+    copy: {
+      fonts: {
+        files: [
+          {
+            expand: true,
+            cwd: 'stylesheets/fonts/',
+            src: ['*'],
+            dest: 'dist/fonts',
+            flatten: true
+          }
+        ]
+      }
+    },
+    clean: ['tmp', 'json', 'dist']
   });
 
   grunt.loadTasks('tasks');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['jshint', 'nodeunit', 'filter', 'compile']);
+  grunt.registerTask('build', ['concat', 'cssmin', 'uglify', 'copy']);
+  grunt.registerTask('default', ['jshint', 'nodeunit', 'filter', 'compile', 'build']);
 };
